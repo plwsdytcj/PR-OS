15a0729daf27603c644c1bb1346c8b9332995e37
Steven Lin <31514970+plwsdytcj@users.noreply.github.com>
2026-06-09 14:54:03 +0800

    Sync login redirect asset version

--- web/static/login.js
+++ web/static/login.js
@@ -50,7 +50,7 @@ async function api(path, options = {}) {
 }
 
 function redirectToApp() {
-  window.location.href = "/app?v=20260609-3";
+  window.location.href = "/app?v=20260609-4";
 }
 
 async function checkExistingSession() {
